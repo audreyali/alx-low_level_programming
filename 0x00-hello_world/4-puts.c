89ed345319819e796e24af0af7c369e24c85fa16
audreyali <audreyali40@gmail.com>
2023-03-16 16:33:33 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This function is void
+ *
+ * Return: whole number
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
