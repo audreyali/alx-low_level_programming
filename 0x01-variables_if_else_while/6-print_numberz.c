aebc5d0c2f492af5e7c9b6c5bf7ef53d18b6a3d5
audreyali <audreyali40@gmail.com>
2023-03-17 08:39:34 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - This is void
+ * Return: whole number
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
