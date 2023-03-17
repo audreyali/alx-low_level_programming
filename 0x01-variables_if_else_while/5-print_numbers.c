0f75fb01ee51c311c9b1ad7a36c24848e1821a31
audreyali <audreyali40@gmail.com>
2023-03-17 08:29:17 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - This function is void
+ * Return: whole number
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
