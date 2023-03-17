83cb649f0cc7dcbf83b7f0da31e69552c78cad14
audreyali <audreyali40@gmail.com>
2023-03-17 03:31:06 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - This function is void
+ * Return: whole number
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)")
+		printf("Size of an int: 4 byte(s)")
+		printf("Size of a long int: 4 byte(s)")
+	printf("Size of long long int: 8 byte(s)")
+	printf("Size of a float: 4 byte(s)")
