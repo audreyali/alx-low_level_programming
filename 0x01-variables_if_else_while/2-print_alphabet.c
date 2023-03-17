c2a9cfd42dcd369d415b64d9b11e958e217af7cc
audreyali <audreyali40@gmail.com>
2023-03-17 07:55:06 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - This function is void
+ * Return: whole number
+ */
+int main(void)
+{
+char i;
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
