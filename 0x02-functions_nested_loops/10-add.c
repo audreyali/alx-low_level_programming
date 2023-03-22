9a7e348cd5aab87b6249a50cae494844bf2149ff
audreyali <audreyali40@gmail.com>
2023-03-22 04:09:55 -0700

    a + b C function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - function that adds two integers
+ * @n4: first number added
+ * @n6: second number to be added
+ * Return: sum
+ */
+
+int add(int n4, int n6)
+{
+	return (n4 + n6);
+}
